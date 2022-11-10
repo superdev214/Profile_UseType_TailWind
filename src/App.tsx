d617fcd492d50ca05b0b55f13cff768a18c3bcc5
superdev214 <116163976+superdev214@users.noreply.github.com>
2022-11-10 23:07:46 +0900

    Profile By TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-red-500">asdf</div>
+      
+    </div>
+  );
+}
+
+export default App;
